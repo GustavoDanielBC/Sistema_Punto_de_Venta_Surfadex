c6334d1c885e63f53e547b045b252107b9f57d86
Daniel-E02 <dani.e02@hotmail.com>
2025-05-10 11:05:09 -0700

    Codificar el Services de Ventas
    Codificar el Services de Ventas para obtener los datos desde la capa BLL (Negocio)

--- AppSistemaVenta/src/app/Services/producto.service.ts
+++ AppSistemaVenta/src/app/Services/producto.service.ts
@@ -9,7 +9,7 @@ import { Producto } from '../Interfaces/producto';
   providedIn: 'root'
 })
 export class ProductoService {
-  private urlApi:string = environment.endpoint + "Rol/";
+  private urlApi:string = environment.endpoint + "Producto/";
 
   constructor(private http:HttpClient) {}
 
