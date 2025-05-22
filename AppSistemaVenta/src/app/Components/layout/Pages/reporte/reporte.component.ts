418cb8dd3e8c9ae0ba741cc0c5c691fa7dcaeefb
GustavoDanielBC <gustavo.barrios@uabc.edu.mx>
2025-05-22 15:01:00 -0700

    PBI_Reportes_US1 Codificar el page para buscar reportes en especifico
    Se codifico el page para implementar las funciones de filtrado de fecha para buscar reportes

--- AppSistemaVenta/src/app/Components/layout/Pages/reporte/reporte.component.ts
+++ AppSistemaVenta/src/app/Components/layout/Pages/reporte/reporte.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import * as moment from 'moment';
+
+import * as XLSX from 'xlsx';
+
+import { Reporte } from 'src/app/Interfaces/reporte';
+import { VentaService } from 'src/app/Services/venta.service';
+import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
+
+export const MY_DATA_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-reporte',
   templateUrl: './reporte.component.html',
-  styleUrls: ['./reporte.component.css']
+  styleUrls: ['./reporte.component.css'],
+  providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_DATA_FORMATS }],
 })
 export class ReporteComponent implements OnInit {
+  formularioFiltro: FormGroup;
+  listaVentasReporte: Reporte[] = [];
+  columnasTabla: string[] = [
+    'fechaRegistro',
+    'numeroVenta',
+    'tipoPago',
+    'total',
+    'producto',
+    'cantidad',
+    'precio',
+    'totalProducto',
+  ];
+  dataVentaReporte = new MatTableDataSource(this.listaVentasReporte);
+  @ViewChild(MatPaginator) paginacionTabla!: MatPaginator;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private _ventaServicio: VentaService,
+    private _utilidadServicio: UtilidadService
+  ) {
+    this.formularioFiltro = this.fb.group({
+      fechaInicio: ['', Validators.required],
+      fechaFin: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.dataVentaReporte.paginator = this.paginacionTabla;
+  }
 }
